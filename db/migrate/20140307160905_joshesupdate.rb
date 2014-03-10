34cad75fb9d99bc048482f59fa40b97fb1205353
el3ctro- <josh.el3@gmail.com>
2014-03-09 20:15:01 -0700

    Fixing

--- db/migrate/20140307160905_joshesupdate.rb
+++ db/migrate/20140307160905_joshesupdate.rb
@@ -1,6 +1,6 @@
 class Joshesupdate < ActiveRecord::Migration
   def change
-  add_column :comments, :user_name, :string
+ # add_column :comments, :user_name, :string
   add_column :posts, :title, :string
   add_column :posts, :link, :string
   
