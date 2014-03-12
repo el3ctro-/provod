52b41a864640681fee28d5891ab1cbf7b8241432
el3ctro- <josh.el3@gmail.com>
2014-03-11 19:56:35 -0700

    fix

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -51,7 +51,7 @@ class PictureUploader < CarrierWave::Uploader::Base
 
  
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
  
 
