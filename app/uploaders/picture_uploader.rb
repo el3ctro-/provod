330314c011f1742e0346a088bd121f7b23be5ddb
el3ctro- <josh.el3@gmail.com>
2014-03-11 20:12:27 -0700

    fix

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -54,6 +54,8 @@ class PictureUploader < CarrierWave::Uploader::Base
   #  "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
 		"#{model.id}"
   end
- 
+ def default_url
+	 "/"
+	 end
 
 end
