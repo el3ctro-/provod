61848fc1e86b4b934f34d5543285a757474c6d32
el3ctro- <josh.el3@gmail.com>
2014-03-19 08:12:51 -0700

    Protected

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,10 +12,7 @@ class PostsController < ApplicationController
     session[:permission] = 0
     if !params.nil? && params[:password] == 'testing'
     session[:permission] = 1
-    elsif params[:id].to_i > 0
     redirect_to :action => 'show', :id => params[:id]
-  elsif params[:id] == 'gallery'
-    redirect_to '/protected_gallery'
   end
   end
 
